4932c11fe272967a6d5e623768194738aa98039d
Viktor Vaidner <space10@mail.ru>
2016-01-04 14:02:26 +0200

    change config

--- app/src/test/java/com/ua/viktor/mytestjenkins/ExampleUnitTest.java
+++ app/src/test/java/com/ua/viktor/mytestjenkins/ExampleUnitTest.java
@@ -3,14 +3,14 @@ package com.ua.viktor.mytestjenkins;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
-@RunWith(RobolectricGradleTestRunner.class)
-@Config(constants = BuildConfig.class, sdk = 21)
+@Config(emulateSdk = 18, reportSdk = 18)
+@RunWith(RobolectricTestRunner.class)
 public class ExampleUnitTest {
     @Test
     public void UserNameTest() {
